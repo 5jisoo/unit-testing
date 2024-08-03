ccd7d010f89c34e69d2e969edad467b3eaecc565
Lucy Oh <56earls@gmail.com>
2024-08-04 03:14:17 +0900

    Add: Chap02 code

---
+++ UnitTesting/Chap02/Classic/CustomerTests.cs
@@ -0,0 +1,36 @@
+﻿namespace UnitTesting.Chap02.Classic;
+
+public class CustomerTests
+{
+    [Fact]
+    public void Purchase_succeeds_when_enough_inventory()
+    {
+        // 준비
+        var store = new Store();
+        store.AddInventory(Product.Shampoo, 10);
+        var customer = new Customer();
+
+        // 실행
+        bool success = customer.Purchase(store, Product.Shampoo, 5);
+
+        // 검증
+        Assert.True(success);
+        Assert.Equal(5, store.GetInventory(Product.Shampoo)); // 상품 재고 5개 감소
+    }
+
+    [Fact]
+    public void Purchase_fails_when_not_enough_inventory()
+    {
+        // 준비
+        var store = new Store();
+        store.AddInventory(Product.Shampoo, 10);
+        var customer = new Customer();
+
+        // 실행
+        bool success = customer.Purchase(store, Product.Shampoo, 15);
+
+        // 검증
+        Assert.False(success);
+        Assert.Equal(10, store.GetInventory(Product.Shampoo)); // 상품 재고 수량 변화 없음
+    }
+}
---
+++ UnitTesting/Chap02/London/CustomerTests.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+
+namespace UnitTesting.Chap02.London;
+
+public class CustomerTests
+{
+    [Fact]
+    public void Purchase_succeeds_when_enough_inventory()
+    {
+        // 준비
+        var storeMock = new Mock<IStore>();
+        storeMock
+            .Setup(x => x.HasEnoughInventory(Product.Shampoo, 5))
+            .Returns(true);
+        var customer = new Customer();
+
+        // 실행
+        bool success = customer.Purchase(storeMock.Object, Product.Shampoo, 5);
+
+        // 검증
+        Assert.True(success);
+        storeMock.Verify(x => x.RemoveInventory(Product.Shampoo, 5), Times.Once);
+    }
+
+    [Fact]
+    public void Purchase_fails_when_not_enough_inventory()
+    {
+        // 준비
+        var storeMock = new Mock<IStore>();
+        storeMock
+            .Setup(x => x.HasEnoughInventory(Product.Shampoo, 5))
+            .Returns(false);
+        var customer = new Customer();
+
+        // 실행
+        bool success = customer.Purchase(storeMock.Object, Product.Shampoo, 5);
+
+        // 검증
+        Assert.False(success);
+        storeMock.Verify(x => x.RemoveInventory(Product.Shampoo, 5), Times.Never);
+    }
+}
